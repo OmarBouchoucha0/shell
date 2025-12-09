a7674918f12f95fb82c4cf974842f3c2d75dacaf
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2025-12-09 19:40:34 +0100

    echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+fn builtin_echo() {
+    println!("echo is a builtin");
 }
+
+fn handle_command(cmd: &str) {
+    match cmd {
+        "echo" => builtin_echo(),
+        _ => println!("{cmd}: not found"),
+    }
+}
+
+fn run() {
+    let mut cmd = String::new();
+    loop {
+        cmd.clear();
+        print!("$ ");
+
+        if let Err(e) = io::stdout().flush() {
+            eprintln!("Error: {e}");
+            continue;
+        }
+
+        if let Err(e) = io::stdin().read_line(&mut cmd) {
+            eprintln!("Error: {e}");
+            continue;
+        }
+
+        let trimmed = cmd.trim();
+        if !trimmed.is_empty() {
+            handle_command(trimmed);
+        }
+    }
+}
+
+fn main() {
+    run();
+}
+
